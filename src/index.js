2fc62dafea91216a904409278cc95f5472a84129
leonardolombardi07 <leonardolombardi@poli.ufrj.br>
2020-06-11 12:30:17 -0300

    Styled components + redux + react-router-dom integrated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Provider from "react-redux";
+import { Provider } from "react-redux";
 
 import { GlobalStyles } from "./constants/styled-components";
+import Router from "./router";
 import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <Provider store={store}></Provider>
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
