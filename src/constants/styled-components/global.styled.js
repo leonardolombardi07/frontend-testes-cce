737231c327019a250edea7a865766e40c4089109
leonardolombardi07 <leonardolombardi@poli.ufrj.br>
2020-06-13 19:46:55 -0300

    Scrollbar wired

--- src/constants/styled-components/global.styled.js
+++ src/constants/styled-components/global.styled.js
@@ -9,6 +9,18 @@ export const GlobalStyles = createGlobalStyle`
     background: #F3F3F3;
 
     font-family: 'Lato', sans-serif;
-    box-sizing: border-box
+    box-sizing: border-box;
+  }
+
+  ::-webkit-scrollbar {
+    width: 9px;
+  }
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: lightgrey;
+    border-radius: 10px;
   }
 `;
