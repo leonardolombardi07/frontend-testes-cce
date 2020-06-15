65c522c14627cfaab8163e17d0b6ff98718e5e2b
leonardolombardi07 <leonardolombardi@poli.ufrj.br>
2020-06-15 13:39:12 -0300

    Updates

---
+++ precache-manifest.24325decce2ae472fbe55df811748ed5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07581641ef58f57bebf9a63e0f8d5b53",
+    "url": "/frontend-testes-cce/index.html"
+  },
+  {
+    "revision": "8d93c23bf972da6aed12",
+    "url": "/frontend-testes-cce/static/js/2.cb7be87a.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/frontend-testes-cce/static/js/2.cb7be87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2134fd80ae2b028856e5",
+    "url": "/frontend-testes-cce/static/js/main.78c32d6a.chunk.js"
+  },
+  {
+    "revision": "cb3e380523e1a4196d65",
+    "url": "/frontend-testes-cce/static/js/runtime-main.b1e5f84c.js"
+  },
+  {
+    "revision": "1f1e1a2920b967f6ff83d29b2ab98454",
+    "url": "/frontend-testes-cce/static/media/add-image-transparent.1f1e1a29.png"
+  },
+  {
+    "revision": "ff26b5274debe0f363e0c6d9939a60d8",
+    "url": "/frontend-testes-cce/static/media/cce-banner-transparent.ff26b527.png"
+  },
+  {
+    "revision": "6fe521f568d9761f50743c3905db182e",
+    "url": "/frontend-testes-cce/static/media/sad-skull.6fe521f5.png"
+  }
+]);
